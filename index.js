c0778e2ae37efba277ad747b9c24574e6a28842a
pparksso <thgml5143@hanmail.net>
2022-08-18 21:36:22 +0900

    기본 구성 완성

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const dotenv = require("dotenv").config();
 const moment = require("moment");
+moment.locale("ko");
 const MongoClient = require("mongodb").MongoClient;
 
 let db = null;
@@ -20,26 +21,42 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "/public")));
-app.get("/diary", (req, res) => {
+app.get("/", (req, res) => {
   res.render("index");
 });
 app.post("/add", (req, res) => {
-  const name = req.body.name;
-  const contents = req.body.contents;
-  const insertData = {
-    name: name,
-    contents: contents,
-  };
-  db.collection("contents").insertOne(insertData, (err, result) => {
-    if (err) {
-      console.log(err, "db insert err");
+  db.collection("count").findOne(
+    {
+      name: "total",
+    },
+    (err, result) => {
+      const no = result.count;
+      const name = req.body.name;
+      const contents = req.body.contents;
+      const sendTime = moment(new Date()).format("YYYY.MM.DD(ddd)");
+      const insertData = {
+        no: no + 1,
+        name: name,
+        contents: contents,
+        time: sendTime,
+      };
+      db.collection("contents").insertOne(insertData, (err, result) => {
+        db.collection("count").updateOne({ name: "total" }, { $inc: { count: 1 } }, (err, result) => {
+          if (err) {
+            console.log(err, "db insert err");
+          }
+        });
+        res.redirect("/list");
+      });
     }
-  });
-  res.redirect("list");
+  );
 });
 app.get("/list", (req, res) => {
-  res.render("list");
-  // db.collection("")
+  db.collection("contents")
+    .find()
+    .toArray((err, result) => {
+      res.render("list", { list: result });
+    });
 });
 app.listen(PORT, () => {
   console.log(`${PORT}포트`);
